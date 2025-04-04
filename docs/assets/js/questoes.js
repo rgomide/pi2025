c8e70b283aade0f66f6c8620e8b6dc754b9e4221
rgomide <renato.s.gomide@gmail.com>
2025-04-04 10:43:45 -0300

    .

--- docs/assets/js/questoes.js
+++ docs/assets/js/questoes.js
@@ -1,73 +1,127 @@
 const questoesMatematica = [
   {
-    "id": 1,
-    "enunciado": "Em um plano cartesiano, qual é a distância entre os pontos A(2,3) e B(5,7)?",
-    "imagem": "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+    "id": 1.0,
+    "enunciado": "Na modelagem e no estudo de fenômenos periódicos, em geral, os modelos associados fazem uso de funções trigonométricas. Nesse sentido, considere um experimento realizado em laboratório, em que uma planta foi colocada em uma estufa, onde a temperatura é controlável. O experimento consiste em observar alterações nas características dessa planta ao ser submetida a variações de temperatura. Durante 24 horas, a temperatura T(x) da estufa variou de acordo com a função T(x) = 20 - 10sen(x) (π.(x/4)), em que x é medido em horas, variando no intervalo 0 ≤ x ≤ 24. Durante esse experimento, quantas vezes a temperatura na estufa atingiu o seu valor mínimo?",
+    "imagem": "",
     "alternativas": [
-      "3 unidades",
-      "4 unidades",
-      "5 unidades",
-      "6 unidades",
-      "7 unidades"
+      "1",
+      "3",
+      "4",
+      "5",
+      "7"
     ],
-    "resposta": "5 unidades"
+    "resposta": "b) 3"
+  },
+  {
+    id: 1.1,
+    enunciado: "Uma mola é solta da posição distendida conforme a figura. A figura à direita representa o gráfico da posição P (em cm) da massa m em função do tempo t (em segundo) em um sistema de coordenadas cartesianas. Esse movimento periódico é descrito por uma expressão do tipo P(t) = ± A cos (ωt) ou P(t) = ± A sen (ωt), em que A > 0 é a amplitude de deslocamento máximo e ω é a frequência, que se relaciona com o período T pela fórmula ω = 2π/T. Considere a ausência de quaisquer forças dissipativas. A expressão algébrica que representa as posições P(t) da massa m, ao longo do tempo, no gráfico, é",
+    imagem: "https://d2q576s0wzfxtl.cloudfront.net/2021/11/30044413/171.png",
+    alternativas: [
+      "3 cos (2t)",
+      "3 sen (2t)",
+      "3 cos (2t)",
+      "6 cos (2t)",
+      "e-6 sen (2t)"
+    ],
+    "resposta": "a) 3 cos (2t)"
+  },
+  {
+    id: 1.2,
+    enunciado: "O mastro de uma bandeira foi instalado perpendicularmente ao solo em uma região plana. Devido aos fortes ventos, três cabos de aço, de mesmo comprimento, serão instalados para dar sustentação ao mastro. Cada cabo de aço ficará perfeitamente esticado, com uma extremidade num ponto P do mastro, a uma altura h do solo, e a outra extremidade, num ponto no chão. Os cabos de aço formam um ângulo a com o plano do chão. Por medida de segurança, há apenas três opções de instalação: Opção I: h = 11m e a = 30°, Opção II: h = 12m e a = 45°, Opção III: h = 18m e a = 60°. A opção a ser escolhida é aquela em que a medida dos cabos seja a menor possível. Qual será a medida, em metro, de cada um dos cabos a serem instalados?",
+    imagem: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+    alternativas: [
+      "22√3/3",
+      "11√2",
+      "12√2",
+      "12√3",
+      "22",
+    ],
+    "resposta": "11√2"
   }
 ]
 
 const questoesFilosofia = [
   {
-    "id": 2,
-    "enunciado": "Qual é a principal contribuição do método cartesiano de Descartes para a filosofia moderna?",
+    "id": 2.0,
+    "enunciado": "(ENEM MEC/2014) É o caráter radical do que se procura que exige a radicalização do próprio processo de busca. Se todo o espaço for ocupado pela dúvida, qualquer certeza que aparecer a partir daí terá sido de alguma forma gerada pela própria dúvida, e não será seguramente nenhuma daquelas que foram anteriormente varridas por essa mesma dúvida. SILVA, F. L. Descartes: a metafísica da modernidade. São Paulo: Moderna, 2001 (adaptado). Apesar de questionar os conceitos da tradição, a dúvida radical da filosofia cartesiana tem caráter positivo por contribuir para o (a)",
     "imagem": "",
     "alternativas": [
-      "A dúvida metódica como caminho para o conhecimento",
-      "A fé como base do conhecimento",
-      "A experiência sensorial como única fonte de verdade",
-      "O relativismo como princípio filosófico",
-      "A negação completa da razão"
+      "fortalecimento dos preconceitos religiosos.",
+      "surgimento do conhecimento inabalável.",
+      "exaltação do pensamento clássico.",
+      "recuperação dos antigos juízos.",
+      "dissolução do saber científico."
     ],
-    "resposta": "A dúvida metódica como caminho para o conhecimento"
+    "resposta": "surgimento do conhecimento inabalável."
   },
   {
-    "id": 3,
-    "enunciado": "Qual a famosa frase de Descartes que resume seu princípio filosófico fundamental?",
+    "id": 2.1,
+    "enunciado": "(ENEM MEC/2015) Após ter examinado cuidadosamente todas as coisas, cumpre enfim concluir e ter por constante que esta proposição, eu sou, eu existo, é necessariamente verdadeira todas as vezes que a enuncio ou que a concebo em meu espírito. DESCARTES, R. Meditações. Pensadores. São Paulo: Abril Cultural, 1979. A proposição “eu sou, eu existo” corresponde a um dos momentos mais importantes na ruptura da filosofia do século XVII com os padrões da reflexão medieval, por",
     "imagem": "",
     "alternativas": [
-      "Penso, logo existo",
-      "Conhece-te a ti mesmo",
-      "O homem é a medida de todas as coisas",
-      "Só sei que nada sei",
-      "O homem é o lobo do homem"
+      "estabelecer o ceticismo como opção legítima.",
+      "utilizar silogismos linguísticos como prova ontológica.",
+      "inaugurar a posição teórica conhecida como empirismo.",
+      "estabelecer um princípio indubitável para o conhecimento.",
+      "questionar a relação entre a filosofia e o tema da existência de Deus."
     ],
-    "resposta": "Penso, logo existo"
+    "resposta": "estabelecer um princípio indubitável para o conhecimento."
+  },
+  {
+    "id": 2.2,
+    "enunciado": "(ENEM 2013) TEXTO I  Há já algum tempo eu me apercebi de que, desde meus primeiros anos, recebera muitas falsas opiniões como verdadeiras, e de que aquilo que depois eu fundei em princípios tão mal assegurados não podia ser senão mui duvidoso e incerto. Era necessário tentar seriamente, uma vez em minha vida, desfazer-me de todas as opiniões a que até então dera crédito, e começar tudo novamente a fim de estabelecer um saber firme e inabalável. DESCARTES, R. Meditações concernentes à Primeira Filosofia. São Paulo: Abril Cultural, 1973 (adaptado). TEXTO II É o caráter radical do que se procura que exige a radicalização do próprio processo de busca. Se todo o espaço for ocupado pela dúvida, qualquer certeza que aparecer a partir daí terá sido de alguma forma gerada pela própria dúvida, e não será seguramente nenhuma daquelas que foram anteriormente varridas por essa mesma dúvida. SILVA, F. L. Descartes: a metafísica da modernidade. São Paulo: Moderna, 2001 (adaptado) A exposição e a análise do projeto cartesiano indicam que, para viabilizar a reconstrução radical do conhecimento, deve-se",
+    "imagem": "",
+    "alternativas": [
+      "retomar o método da tradição para edificar a ciência com legitimidade.",
+      "questionar de forma ampla e profunda as antigas ideias e concepções.",
+      "investigar os conteúdos da consciência dos homens menos esclarecidos.",
+      "buscar uma via para eliminar da memória saberes antigos e ultrapassados.",
+      "encontrar ideias e pensamentos evidentes que dispensam ser questionados."
+    ],
+    "resposta": "questionar de forma ampla e profunda as antigas ideias e concepções."
   }
 ]
 
 const questoesHistoria = [
   {
-    "id": 6,
-    "enunciado": "Em qual período histórico René Descartes desenvolveu suas principais obras?",
+    "id": 3.0,
+    "enunciado": "(ENEM MEC/2014) É o caráter radical do que se procura que exige a radicalização do próprio processo de busca. Se todo o espaço for ocupado pela dúvida, qualquer certeza que aparecer a partir daí terá sido de alguma forma gerada pela própria dúvida, e não será seguramente nenhuma daquelas que foram anteriormente varridas por essa mesma dúvida. SILVA, F. L. Descartes: a metafísica da modernidade. São Paulo: Moderna, 2001 (adaptado). Apesar de questionar os conceitos da tradição, a dúvida radical da filosofia cartesiana tem caráter positivo por contribuir para o (a)",
     "imagem": "",
     "alternativas": [
-      "Idade Média",
-      "Idade Moderna",
-      "Antiguidade Clássica",
-      "Período Contemporâneo",
-      "Renascimento"
+      "fortalecimento dos preconceitos religiosos.",
+      "surgimento do conhecimento inabalável.",
+      "exaltação do pensamento clássico.",
+      "recuperação dos antigos juízos.",
+      "dissolução do saber científico."
     ],
-    "resposta": "Idade Moderna"
+    "resposta": "recuperação dos antigos juízos."
   },
   {
-    "id": 7,
-    "enunciado": "Qual obra de Descartes é considerada um marco do pensamento moderno?",
+    "id": 3.1,
+    "enunciado": "(Enem 2013) TEXTO I Há já algum tempo eu me apercebi de que, desde meus primeiros anos, recebera muitas falsas opiniões como verdadeiras, e de que aquilo que depois eu fundei em princípios tão mal assegurados não podia ser senão mui duvidoso e incerto. Era necessário tentar seriamente, uma vez em minha vida, desfazer-me de todas as opiniões a que até então dera crédito, e começar tudo novamente a fim de estabelecer um saber firme e inabalável. DESCARTES, R. Meditações concernentes à Primeira Filosofia. São Paulo: Abril Cultural, 1973 (adaptado). TEXTO II É o caráter radical do que se procura que exige a radicalização do próprio processo de busca. Se todo o espaço for ocupado pela dúvida, qualquer certeza que aparecer a partir daí terá sido de alguma forma gerada pela própria dúvida, e não será seguramente nenhuma daquelas que foram anteriormente varridas por essa mesma dúvida. SILVA, F. L. Descartes: a metafísica da modernidade. São Paulo: Moderna, 2001 (adaptado) A exposição e a análise do projeto cartesiano indicam que, para viabilizar a reconstrução radical do conhecimento, deve-se",
     "imagem": "",
     "alternativas": [
-      "Discurso do Método",
-      "A República",
-      "Leviatã",
-      "O Príncipe",
-      "Suma Teológica"
+      "retomar o método da tradição para edificar a ciência com legitimidade.",
+      "questionar de forma ampla e profunda as antigas ideias e concepções.",
+      "investigar os conteúdos da consciência dos homens menos esclarecidos.",
+      "buscar uma via para eliminar da memória saberes antigos e ultrapassados.",
+      "encontrar ideias e pensamentos evidentes que dispensam ser questionados."
     ],
-    "resposta": "Discurso do Método"
+    "resposta": "questionar de forma ampla e profunda as antigas ideias e concepções."
+  },
+  {
+    "id": 3.2,
+    "enunciado": "(ENEM MEC/2015) Após ter examinado cuidadosamente todas as coisas, cumpre enfim concluir e ter por constante que esta proposição, eu sou, eu existo, é necessariamente verdadeira todas as vezes que a enuncio ou que a concebo em meu espírito.DESCARTES, R. Meditações. Pensadores. São Paulo: Abril Cultural, 1979.A proposição “eu sou, eu existo” corresponde a um dos momentos mais importantes na ruptura da filosofia do século XVII com os padrões da reflexão medieval, po",
+    "imagem": "",
+    "alternativas": [
+      "estabelecer o ceticismo como opção legítima.",
+      "utilizar silogismos linguísticos como prova ontológica.",
+      "inaugurar a posição teórica conhecida como empirismo.",
+      "estabelecer um princípio indubitável para o conhecimento.",
+      "questionar a relação entre a filosofia e o tema da existência de Deus."
+    ],
+    "resposta": "utilizar silogismos linguísticos como prova ontológica."
   }
 ]
+
+
